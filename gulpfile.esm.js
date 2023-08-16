eff4830b30032c8cecb4d02f24e8e2de560e058e
Zokerzes <yozhik13@gmail.com>
2023-08-16 19:44:21 +0300

    ++

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -1,10 +1,18 @@
 import { src, dest } from 'gulp';
+import pug from 'gulp-pug'
 
 const
   SRC = './src/',
   DEST = './_dest/';
 
+export function html() {
+  return src(SRC + '*.pug')
+    .pipe(pug())
+    .pipe(dest(DEST));
+}
+
 export function js() {
   return src(SRC + '*.js')
     .pipe(dest(DEST));
 }
+
